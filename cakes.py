286380b9d8f1082da660c872a6882e5210181ab7
Angel Jackson <136288983+amjackson656@users.noreply.github.com>
2023-07-20 16:36:31 -0400

    Create cakes.py

---
+++ cakes.py
@@ -0,0 +1,6 @@
+from desserts import Desserts
+
+class Cake(Desserts):
+    cake_flavor = ["vanilla", "chocolate", "carrot", "confetti"]
+    cake_icing = ["buttercream", "cream cheese", "whipped"]
+    cake_topping = ["sprinkles", "rock candy", "chocolate shavings", "none"]
