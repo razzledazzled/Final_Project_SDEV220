02b602fa01f4e738747da0fd2441ed1efa15a3e6
Anthony <harrisanthonychs@gmail.com>
2023-07-28 20:24:47 -0500

    Merged Anthony-Dev branch to Master

--- classes.py
+++ classes.py
@@ -1,35 +1,145 @@
-# Define your classes in this file
-
-class MyClass:
-    def __init__(self, name):
-        self.name = name
-
-    def greet(self):
-        print(f"Hello, {self.name}!")
-
-#Sample Dessert super class
 class Dessert:
-    def __init__(self, flavor, price):
-        self.flavor = flavor
+    def __init__(self, name, price):
+        self.name = name
         self.price = price
 
+    def __str__(self):
+        return f"{self.name} - ${self.price:.2f}"
 
-class Pie(Dessert):
-    def __init__(self, flavor, price, filling_type, crust_type):
-        super().__init__(flavor, price)
-        self.filling_type = filling_type
-        self.crust_type = crust_type
+class Pie:
+    def __init__(self):
+        # Default values for a basic pie
+        self.crust_type = "Regular"
+        self.size = "Small"
+        self.filling = "Apple"
+        self.price = 10.00  # Base price for a basic pie
 
+    def set_crust_type(self, crust_type):
+        # Set the crust type and update the price accordingly
+        if crust_type == "Regular":
+            self.crust_type = "Regular"
+            self.price += 0.00
+        elif crust_type == "Graham Cracker":
+            self.crust_type = "Graham Cracker"
+            self.price += 2.00
+        elif crust_type == "Chocolate":
+            self.crust_type = "Chocolate"
+            self.price += 3.00
+        else:
+            print("Invalid crust type.")
 
-class Cake(Dessert):
-    def __init__(self, flavor, price, frosting_type, layers):
-        super().__init__(flavor, price)
-        self.frosting_type = frosting_type
-        self.layers = layers
+    def set_size(self, size):
+        # Set the pie size and update the price accordingly
+        if size == "Small":
+            self.size = "Small"
+            self.price += 0.00
+        elif size == "Medium":
+            self.size = "Medium"
+            self.price += 5.00
+        elif size == "Large":
+            self.size = "Large"
+            self.price += 10.00
+        else:
+            print("Invalid size.")
 
+    def set_filling(self, filling):
+        # Set the pie filling and update the price accordingly
+        if filling == "Apple":
+            self.filling = "Apple"
+            self.price += 0.00
+        elif filling == "Cherry":
+            self.filling = "Cherry"
+            self.price += 2.00
+        elif filling == "Blueberry":
+            self.filling = "Blueberry"
+            self.price += 3.00
+        elif filling == "Pumpkin":
+            self.filling = "Pumpkin"
+            self.price += 3.50
+        else:
+            print("Invalid filling.")
 
-class Cookie(Dessert):
-    def __init__(self, flavor, price, size, texture):
-        super().__init__(flavor, price)
-        self.size = size
-        self.texture = texture
+    def get_price(self):
+        # Return the final price of the customized pie
+        return self.price
+
+class Cake():
+    def __init__(self):
+        # Default values for a basic cake
+        self.flavor = "Vanilla"
+        self.size = "Small"
+        self.icing = "White Icing"
+        self.toppings = "None"
+        self.layers = 1
+        self.price = 16.00  # Base price for a basic Cake
+
+    def set_flavor(self, flavor):
+    # Set the flavor type and update the price accordingly
+        if flavor == "Vanilla":
+            self.flavor = "Vanilla"
+            self.price += 0.00
+        elif flavor == "Chocolate":
+            self.flavor = "Chocolate"
+            self.price += 2.00
+        elif flavor == "Carrot":
+            self.flavor = "Carrot"
+            self.price += 3.00
+        elif flavor == "New York Cheesecake":
+            self.flavor = "New York Cheescake"
+            self.price += 5.00
+        else:
+            print("Invalid crust type.")
+
+    def set_size(self, size):
+        # Set the size and update the price accordingly
+        if size == "Small":
+            self.size = "Small"
+            self.price += 0.00
+        elif size == "Medium":
+            self.size = "Medium"
+            self.price += 0.5
+        elif size == "Large":
+            self.size = "Large"
+            self.price = 1.00
+        else:
+            print("Invalid crust type.")
+
+    def set_icing(self, icing):
+        # Set the icing and update the price accordingly
+        if icing == "White Icing":
+            self.icing = "White Icing"
+            self.price += 0.00
+        elif icing == "Chocolate":
+            self.icing = "Chocolate"
+            self.price += 0.5
+        elif icing == "Berry":
+            self.icing = "Berry"
+            self.price = 1.00
+        else:
+            print("Invalid icing type.")
+
+    def set_toppings(self, toppings):
+        # Set the icing and update the price accordingly
+        if toppings == "White Icing":
+            self.toppings = "White Icing"
+            self.price += 0.00
+        elif toppings == "Chocolate":
+            self.toppings = "Chocolate"
+            self.price += 0.5
+        elif toppings == "Berry":
+            self.toppings = "Berry"
+            self.price = 1.00
+        else:
+            print("Invalid icing type.")
+
+    def get_price(self):
+        # Return the final price of the customized cake
+        return self.price
+
+class Cookies(Dessert):
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price)
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"{super().__str__()}, Quantity: {self.quantity}"
--- main.py
+++ main.py
@@ -1,20 +1,139 @@
-# Import the necessary classes and modules
-from tkinter import Tk, Button
-from classes import MyClass
+import tkinter as tk
+from tkinter import messagebox
+from classes import Pie
+from classes import Cake
 
-# Create an instance of the class
-my_object = MyClass("John")
+class DessertOrderApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Pie or Die: Customize Your Order")
+        
+        self.cart = []  # List to store the selected desserts
+        
+        self.create_widgets()
 
-# Define a function to execute on button click
-def button_click():
-    my_object.greet()
+    def create_widgets(self):
+        # Create the buttons for each dessert type
+        pie_button = tk.Button(self.root, text="Build Your Pie", command=self.open_pie_customization)
+        pie_button.pack()
 
-# Create the GUI window
-window = Tk()
+        cake_button = tk.Button(self.root, text="Customize Your Cake", command=self.open_cake_customization)
+        cake_button.pack()
 
-# Create a button that calls the function on click
-button = Button(window, text="Click Me!", command=button_click)
-button.pack()
+        # Create a cart button to view the cart
+        cart_button = tk.Button(self.root, text="View Cart", command=self.view_cart)
+        cart_button.pack(side='right', anchor='ne', padx=10, pady=10)
 
-# Start the Tkinter event loop
-window.mainloop()
+    def open_pie_customization(self):
+        # Create a new subwindow for customizing the pie
+        pie_customization_window = tk.Toplevel(self.root)
+        pie_customization_window.title("Customize Pie")
+
+        # Set the width and height of the window
+        pie_customization_window.geometry("400x200")
+
+        # Create a new instance of the Pie class for customization
+        pie_instance = Pie()
+
+        # Create a variable to store the selected options
+        crust_var = tk.StringVar()
+        size_var = tk.StringVar()
+        filling_var = tk.StringVar()
+
+        # Function to add the customized pie to the cart
+        def add_to_cart():
+            pie_instance.set_crust_type(crust_var.get())
+            pie_instance.set_size(size_var.get())
+            pie_instance.set_filling(filling_var.get())
+            self.cart.append(pie_instance)
+            messagebox.showinfo("Success", "Pie added to cart!")
+            # Close the "Customize Pie" subwindow after adding to the cart
+            pie_customization_window.destroy()
+
+        # Create option menus for each pie attribute
+        crust_label = tk.Label(pie_customization_window, text="Crust Type:")
+        crust_label.pack()
+        crust_menu = tk.OptionMenu(pie_customization_window, crust_var, "Regular", "Graham Cracker", "Chocolate")
+        crust_menu.pack()
+
+        size_label = tk.Label(pie_customization_window, text="Size:")
+        size_label.pack()
+        size_menu = tk.OptionMenu(pie_customization_window, size_var, "Small", "Medium", "Large")
+        size_menu.pack()
+
+        filling_label = tk.Label(pie_customization_window, text="Filling:")
+        filling_label.pack()
+        filling_menu = tk.OptionMenu(pie_customization_window, filling_var, "Apple", "Cherry", "Blueberry", "Pumpkin")
+        filling_menu.pack()
+
+        # Button to add the customized pie to the cart
+        add_to_cart_button = tk.Button(pie_customization_window, text="Add to Cart", command=add_to_cart)
+        add_to_cart_button.pack()
+
+    def open_cake_customization(self):
+        # Create a new subwindow for customizing the cake
+        cake_customization_window = tk.Toplevel(self.root)
+        cake_customization_window.title("Customize Cake")
+
+        # Set the width and height of window
+        cake_customization_window.geometry("400x200")
+
+        # Create a new instance of the Cake class for customization
+        cake_instance = Cake()
+
+        # Create a variable to store the selected options
+        flavor_var = tk.StringVar()
+        size_var = tk.StringVar()
+        icing_var = tk.StringVar()
+        toppings_var = tk.StringVar()
+        layers_var = tk.StringVar()
+
+        # Function to add the cusomized cake to the chart
+        def add_to_cart():
+            cake_instance.set_flavor(flavor_var.get())
+            #cake_instance.set_size(size_var.get()) #HERE WHEN NEW CLASS STUFF IS ADDED
+            self.cart.append(cake_instance)
+            messagebox.showinfo("Success", "Cake added to cart!")
+            # Close the "Customize Cake" subwindow after adding to the cart
+            cake_customization_window.destroy()
+        
+        #Create option menus for each cake attribute
+        flavor_label = tk.Label(cake_customization_window, text="Flavor Type:")
+        flavor_label.pack()
+        flavor_menu = tk.OptionMenu(cake_customization_window, flavor_var, "Vanilla", "Chocolate", "Carrot", "New York Cheesecake")
+        flavor_menu.pack()
+        size_label = tk.Label(cake_customization_window, text="Size Type:")
+        size_label.pack()
+        size_menu = tk.OptionMenu(cake_customization_window, size_var, "Small", "Medium", "Large")
+        
+        # Button to add the customized pie to the cart
+        add_to_cart_button = tk.Button(cake_customization_window, text="Add to Cart", command=add_to_cart)
+        add_to_cart_button.pack()
+
+    def view_cart(self):
+        # Display the selected desserts in the cart
+        if not self.cart:
+            messagebox.showinfo("Empty Cart", "Your cart is empty.")
+        else:
+            cart_items = []
+            for dessert in self.cart:
+                if isinstance(dessert, Pie):
+                    cart_items.append(f"{dessert.crust_type} Pie ({dessert.size}, {dessert.filling}): ${dessert.get_price():.2f}")
+                elif isinstance(dessert, Cake):
+                    # cart_items.append(f"{dessert.flavor} ")
+                    cart_items.append(f"{dessert.flavor} Cake: ({dessert.size}) ${dessert.get_price():.2f}")
+                # Add more elif statements for other dessert types if needed
+
+        cart_text = "\n".join(cart_items)
+        messagebox.showinfo("Cart", f"Cart items:\n{cart_text}")
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = DessertOrderApp(root)
+
+    # Set the width and height of the main window
+    root.geometry("400x300")
+
+    root.mainloop()
